68cc79d7acf2310352abc094f2e228871b29d8fe
Juan Carlos <jc.jiron@casai.com>
2022-10-04 13:03:56 -0500

    test: Adding Jest

--- src/components/amenities/amenities.spec.tsx
+++ src/components/amenities/amenities.spec.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render } from "@testing-library/react";
 
-import Amenities from './amenities';
+import { Amenities } from "./amenities";
+import Button from "../button/index";
 
-describe('Amenities', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Amenities />);
+describe("Amenities", () => {
+  it("should render successfully", () => {
+    const { baseElement } = render(
+      <Amenities images={[]} Button={<Button />} />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
